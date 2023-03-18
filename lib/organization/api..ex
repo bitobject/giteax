a50b3e8e9d7ae1056023a8840561b03dc10acc01
One Object <haxttanak@gmail.com>
2023-03-18 12:43:34 +0400

    Add doc and examples to create repo

--- lib/organization/api..ex
+++ lib/organization/api..ex
@@ -7,7 +7,20 @@ defmodule Giteax.Organization.Api do
   alias Giteax.Helper
 
   @doc """
-  params = [org: org]
+  Create a repository.
+
+  Returns `Tesla.Env.result()`.
+
+  ## Required Options
+  * `:org` - the organization's name.
+
+  ## Examples
+
+      iex> create_org_repo(%Tesla.Client{}, %RepoRequestParams{}, [org: "org"])
+      {:ok, %Tesla.Env{}}
+
+      iex> create_org_repo(%Tesla.Client{}, %RepoRequestParams{}, [org: "bad_org"])
+      {:error, error}
   """
   @spec create_org_repo(Tesla.Client.t(), RepoRequestParams.t(), Keyword.t()) :: Tesla.Env.result()
   def create_org_repo(client, %RepoRequestParams{} = body, params) do
